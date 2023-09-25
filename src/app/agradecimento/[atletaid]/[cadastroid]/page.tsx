c96edf976f1ca4bd484b1839e9156fbb90d1db3a
Thiago Dutra <thiago.dutra@grupoboticario.com.br>
2023-09-25 11:03:45 -0300

    feat: 🎸 Insert rules server

--- src/app/agradecimento/[atletaid]/[cadastroid]/page.tsx
+++ src/app/agradecimento/[atletaid]/[cadastroid]/page.tsx
@@ -1,67 +1,16 @@
-"use client";
-import { useEffect } from "react";
-import Image from "next/image";
+
 import prismadb from "@/utils/prismadb";
-import axios from "axios";
+import AgradecimentoClient from "../components/page";
 
-const AgredecimentoId = ({ response, response2 }: any) => {
-  const gerarQRCode = async () => {
-    const newData = {
-      amount: response2?.valorIncentivo,
-      city: "Campo Mourão",
-      key: response?.chavePix,
-      key_type: response?.tipoChave,
-      name: response?.nomeAtleta,
-    };
-    const api = await axios.post(
-      "https://www.gerarpix.com.br/emvqr-static",
-      newData
-    );
-    console.log("api ", api);
+interface AtletaId {
+  params: {
+    atletaid: string;
+    cadastroid: string;
   };
+}
 
-  return (
-    <main className="h-screen bg-primary flex flex-col justify-center items-center">
-      <div className="relative sm:mt-24 w-[200px] sm:w-[300px] sm:h-[300px] h-[200px]">
-        <Image
-          src="/images/star-outline.png"
-          fill
-          alt="imagem de estrelas"
-          className="absolulte animate-wiggle"
-        />
-        <Image
-          src="/images/trofeu.png"
-          fill
-          alt="imagem de trofeus"
-          className="absolute top-0 animate-bounce"
-        />
-      </div>
-      <h1 className="font-title text-7xl sm:text-[110px] text-white leading-none mt-7">
-        Obrigado pelo apoio!!
-      </h1>
-      <p className="text-white text-center">
-        Atletas como eu sonham em poder viver do esporte, saiba que{" "}
-        <b>
-          você é<br />
-          uma peça essencial
-        </b>
-        para que isso possa se tornar realidade!
-      </p>
-      <button
-        className="py-6 px-14 bg-secondary text-primary uppercase border border-primary hover:bg-white hover:text-black mt-9 ease-in-out duration-200"
-        type="button"
-        onClick={() => gerarQRCode()}
-      >
-        Finalizar pagamento
-      </button>
-    </main>
-  );
-};
-
-export default AgredecimentoId;
-
-export async function getServerSidePropss(context: any) {
-  const { atletaid, cadastroid } = context.params;
+const AgredecimentoId = async({params}: AtletaId) => {
+  const { atletaid, cadastroid } = params;
 
   const response = await prismadb.campanha.findUnique({
     where: {
@@ -74,11 +23,13 @@ export async function getServerSidePropss(context: any) {
       id: cadastroid,
     },
   });
+  
 
-  return {
-    props: {
-      response,
-      response2,
-    },
-  };
-}
+  return (
+   <AgradecimentoClient response={response} response2={response2} />
+  );
+};
+
+export default AgredecimentoId;
+
+
---
+++ src/app/agradecimento/[atletaid]/components/page.tsx
@@ -0,0 +1,67 @@
+"use client"
+import React, { useEffect } from "react";
+import Image from "next/image";
+import axios from "axios";
+
+const AgradecimentoClient = ({ response, response2 }: any) => {
+ console.log("response2 ", response2);
+ console.log("response ", response);
+
+  const gerarQRCode = async () => {
+    const newData = {
+      amount: response2?.valorIncentivo,
+      city: "Campo Mourão",
+      key: response?.chavePix,
+      key_type: response?.tipoChave,
+      name: response?.nomeAtleta,
+    };
+    const api = await axios.post(
+      "https://www.gerarpix.com.br/emvqr-static",
+      newData
+    );
+    console.log("api ", api);
+  };
+
+  useEffect(() => {
+    gerarQRCode()
+  }, [response, response2])
+  
+  return (
+    <main className="h-screen bg-primary flex flex-col justify-center items-center">
+      <div className="relative sm:mt-24 w-[200px] sm:w-[300px] sm:h-[300px] h-[200px]">
+        <Image
+          src="/images/star-outline.png"
+          fill
+          alt="imagem de estrelas"
+          className="absolulte animate-wiggle"
+        />
+        <Image
+          src="/images/trofeu.png"
+          fill
+          alt="imagem de trofeus"
+          className="absolute top-0 animate-bounce"
+        />
+      </div>
+      <h1 className="font-title text-7xl sm:text-[110px] text-white leading-none mt-7">
+        Obrigado pelo apoio!!
+      </h1>
+      <p className="text-white text-center">
+        Atletas como eu sonham em poder viver do esporte, saiba que{" "}
+        <b>
+          você é<br />
+          uma peça essencial
+        </b>
+        para que isso possa se tornar realidade!
+      </p>
+      {/* <button
+        className="py-6 px-14 bg-secondary text-primary uppercase border border-primary hover:bg-white hover:text-black mt-9 ease-in-out duration-200"
+        type="button"
+        onClick={gerarQRCode}
+      >
+        Finalizar pagamento
+      </button> */}
+    </main>
+  );
+};
+
+export default AgradecimentoClient;
